144a2a21fa97d7f2a561bc81828939ae06e2914a
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-09-20 23:21:05 +0530

    'ip_address_changed'

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
-//import express;
-
-// (after npm install express)
 const express = require('express');
 
 const path = require('path');
 
 console.log("There is no god, well, I can't say. Something is there...");
 
-
-//
-// // create new express app and save it as "app"
 const app = express();
-//
+
 // // server configuration
 app.use('/static', express.static('public'))
 
 
 const PORT = 8080;
-//
-// // create a route for the app
-//
-var  Gpio = require('onoff').Gpio;
-var LED = new Gpio(4,'out');
+// var Gpio = require('onoff').Gpio;
+// var LED = new Gpio(4, 'out');
 
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-	//let {deviceId,val} = req.query;
-	//console.log(`Received request - Turn Device ID - ${deviceId} ${val==1?'ON':'OFF'}`);
-        //LED.writeSync(parseInt(val));
-	//res.send('Hello World, Pi here');
-        res.sendFile(path.join(__dirname,'/public/index.html'));
+	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.post('/changeState',(req,res) => {
-	let {deviceId,val} = req.body;
-	console.log(`Received request - Turn Device ID - ${deviceId} ${val==1?'ON':'OFF'}`);
-	LED.writeSync(parseInt(val));
+app.post('/changeState', (req, res) => {
+	let { deviceId, val } = req.body;
+	console.log(`Received request - Turn Device ID - ${deviceId} ${val == 1 ? 'ON' : 'OFF'}`);
 	res.send(JSON.stringify({
-		'message' : 'device state changed successfully :)'
+		'message': 'device state changed successfully :)'
 	}));
 
 });
 
 //
-//   // make the server listen to requests
+// making the server to listen to requests
 app.listen(PORT, () => {
-     console.log(`Server running at: http://localhost:${PORT}/`);
-     });
+	console.log(`Server running at: http://localhost:${PORT}/`);
+});
