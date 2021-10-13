1aa941098e8d762b8c61ea765e805528f87e8b82
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-10-13 23:42:29 +0530

    code_refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const path = require('path');
 
-// const { exec } = require('child_process');
+const { exec } = require('child_process');
 
 console.log("There is no god, well, I can't say. Something is there...");
 
@@ -13,60 +13,44 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = 8080;
-// var Gpio = require('onoff').Gpio;
-// var LED = new Gpio(4, 'out');
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
 
 app.use(express.json());
 
-// LED.writeSync(1);
+LED.writeSync(1);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.post('/changeState', (req, res) => {
-
-	// exec(`sudo fswebcam /home/ubuntu/HomeAuto/HomeAuto/public/image.jpg -r "600x600"`, (error, stdout, stderr) => {
-	// 	if (error) {
-	// 		console.log(`error: ${error.message}`);
-	// 		return;
-	// 	}
-	// 	if (stderr) {
-	// 		console.log(`stderr: ${stderr}`);
-	// 		return;
-	// 	}
-	// 	// console.log(`stdout: ${stdout}`);
-	// });
-
-
-	// let { deviceId, val } = req.body;
-	// console.log(`Received request - Turn Device ID - ${deviceId} ${val == 1 ? 'ON' : 'OFF'}`);
-	// val = val == 1 ? 0 : 1;
-	// LED.writeSync(parseInt(val));
+	let { deviceId, val } = req.body;
+	console.log(`Received request - Turn Device ID - ${deviceId} ${val == 1 ? 'ON' : 'OFF'}`);
+	val = val == 1 ? 0 : 1;
+	LED.writeSync(parseInt(val));
 	res.send(JSON.stringify({
 		'message': 'device switched successfully :)'
 	}));
-
 });
 
 app.post('/captureImage', (req, res) => {
-	// exec(`sudo fswebcam /home/ubuntu/HomeAuto/HomeAuto/public/image.jpg -r "600x600"`, (error, stdout, stderr) => {
-	// 	if (error) {
-	// 		console.log(`error: ${error.message}`);
-	// 		return;
-	// 	}
-	// 	if (stderr) {
-	// 		console.log(`stderr: ${stderr}`);
-	// 		return;
-	// 	}
-	// 	// console.log(`stdout: ${stdout}`);
-	// });
+	exec(`sudo fswebcam /home/ubuntu/HomeAuto/HomeAuto/public/image.jpg -r "600x600"`, (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		// console.log(`stdout: ${stdout}`);
+	});
 	res.send(JSON.stringify({
 		'message': 'image captured successfully :)'
 	}));
 })
 
-//
 // making the server to listen to requests
 app.listen(PORT, () => {
 	console.log(`Server running at: http://localhost:${PORT}/`);
@@ -75,7 +59,7 @@ app.listen(PORT, () => {
 
 process.on('SIGINT', function (code) {
 	console.log('terminating node server');
-	// LED.writeSync(1);
+	LED.writeSync(1);
 	// LED.unexport(); // Unexport GPIO to free resources
 	process.exit(1)
 });
