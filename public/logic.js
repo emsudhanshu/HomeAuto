1e108a39603df4aa5e2932464262c600c8228b79
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-10-14 01:21:03 +0530

    syncFileresolved

--- public/logic.js
+++ public/logic.js
@@ -1,5 +1,10 @@
 import { switchDevice, capturePic } from "./modules/features.js";	
 
+window.addEventListener("DOMContentLoaded", () => {
+	// DOM ready! Images, frames, and other subresources are still downloading.
+	capturePic();
+});
+
 // DESK CONTROLS
 document.querySelector('#d1_on').addEventListener('click', function(){switchDevice(1,1)})
 document.querySelector('#d1_off').addEventListener('click', function(){switchDevice(0,1)})
