e0f40331a0b62a52c002944c7821dcfd19e8c775
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-10-18 20:30:25 +0530

    reactroutingfixed

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ const app = express();
 
 // // server configuration
 
-app.use(express.static(path.join(__dirname, "public","home_auto","build")));
-app.use(express.static("public"));
+app.use(express.static("public/home_auto/build"));
+// app.use(express.static("public"));
 
 
 const PORT = 8080;
-var Gpio = require('onoff').Gpio;
-var LED = new Gpio(4, 'out');
+// var Gpio = require('onoff').Gpio;
+// var LED = new Gpio(4, 'out');
 
 app.use(express.json());
 
-LED.writeSync(1);
+// LED.writeSync(1);
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html');
+	res.sendFile(__dirname + "/public/home_auto/build/index.html");
 });
 
 app.post('/changeState', (req, res) => {
-	let { deviceId, val } = req.body;
-	console.log(`Received request - Turn Device ID - ${deviceId} ${val == 1 ? 'ON' : 'OFF'}`);
-	val = val == 1 ? 0 : 1;
+	// let { deviceId, val } = req.body;
+	// console.log(`Received request - Turn Device ID - ${deviceId} ${val == 1 ? 'ON' : 'OFF'}`);
+	// val = val == 1 ? 0 : 1;
 	LED.writeSync(parseInt(val));
 	res.send(JSON.stringify({
 		'message': 'device switched successfully :)'
@@ -39,18 +39,18 @@ app.post('/changeState', (req, res) => {
 });
 
 app.post('/capturePic', (req, res) => {
-	execSync(`sudo fswebcam /home/ubuntu/HomeAuto/HomeAuto/image.jpg -r "600x600"`, (error, stdout, stderr) => {
-		if (error) {
-			console.log(`error: ${error.message}`);
-			return;
-		}
-		if (stderr) {
-			console.log(`stderr: ${stderr}`);
-			return;
-		}
-	});
-	const content = fs.readFileSync('./image.jpg', { encoding: 'base64' });
-	res.send(JSON.stringify({ message: 'image captured successfully', imageData: content }))
+	// execSync(`sudo fswebcam /home/ubuntu/HomeAuto/HomeAuto/image.jpg -r "600x600"`, (error, stdout, stderr) => {
+	// 	if (error) {
+	// 		console.log(`error: ${error.message}`);
+	// 		return;
+	// 	}
+	// 	if (stderr) {
+	// 		console.log(`stderr: ${stderr}`);
+	// 		return;
+	// 	}
+	// });
+	// const content = fs.readFileSync('./image.jpg', { encoding: 'base64' });
+	// res.send(JSON.stringify({ message: 'image captured successfully', imageData: content }))
 	// console.log(`stdout: ${stdout}`);
 
 })
@@ -63,7 +63,7 @@ app.listen(PORT, () => {
 
 process.on('SIGINT', function (code) {
 	console.log('terminating node server');
-	LED.writeSync(1);
+	// LED.writeSync(1);
 	// LED.unexport(); // Unexport GPIO to free resources
 	process.exit(1)
 });
