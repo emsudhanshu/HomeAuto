7e3ed5054a932a029cd14585ed1944f357d8d831
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-10-21 12:42:14 +0530

    AUTO-PUSHED

--- public/home_auto/src/components/pages/DeskController/DeskController.js
+++ public/home_auto/src/components/pages/DeskController/DeskController.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './DeskController.css';
-// import GATEWAY from '../../../config/gateway.js';
-// import { CHANGE_STATE_ENDPOINT, CAPTURE_PIC_ENDPOINT } from '../../../config/endpoints.js';
 import {switchDevice, capturePic} from '../../../modules/features';
 
 export default function DeskController() {
@@ -12,28 +10,6 @@ export default function DeskController() {
         capturePic(1,setShowLoader)
     }, [])
 
-    // function switchDevice(val, deviceId) {
-    //     setShowLoader(true);
-    //     fetch(`${GATEWAY}${CHANGE_STATE_ENDPOINT}`,
-    //         {
-    //             method: 'POST', body: JSON.stringify({ val, deviceId }),
-    //             headers: { 'Content-Type': 'application/json' }
-
-    //         })
-    //         .then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data); alert(`success - device 1 turned ${val == 1 ? 'on' : 'off'}`) }).catch(() => setShowLoader(false));
-    // }
-
-    // function capturePic(cameraId) {
-
-    //     setShowLoader(true);
-
-    //     fetch(`${GATEWAY}${CAPTURE_PIC_ENDPOINT}`,
-    //         {
-    //             method: 'POST', body: JSON.stringify({ cameraId }),
-    //             headers: { 'Content-Type': 'application/json' }
-    //         }).then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data.body); document.querySelector('#c1_view').src = `data:image/png;base64,${data.imageData}` }).catch(() => setShowLoader(false));
-
-    // }
     return (
         <React.Fragment>
             {showLoader &&
