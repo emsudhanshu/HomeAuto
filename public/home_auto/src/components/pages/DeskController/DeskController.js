84c0945beb30851c934d4a4ad202eb1b60ea89af
emsudhanshu <kakkar.sudhanshu@tcs.com>
2021-10-21 11:04:30 +0530

    AUTO-PUSHED

--- public/home_auto/src/components/pages/DeskController/DeskController.js
+++ public/home_auto/src/components/pages/DeskController/DeskController.js
@@ -1,38 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import './DeskController.css';
-import GATEWAY from '../../../config/gateway.js';
-import { CHANGE_STATE_ENDPOINT, CAPTURE_PIC_ENDPOINT } from '../../../config/endpoints.js';
+// import GATEWAY from '../../../config/gateway.js';
+// import { CHANGE_STATE_ENDPOINT, CAPTURE_PIC_ENDPOINT } from '../../../config/endpoints.js';
+import {switchDevice, capturePic} from '../../../modules/features';
 
 export default function DeskController() {
 
     const [showLoader, setShowLoader] = useState(true);
 
     useEffect(() => {
-        capturePic(1)
+        capturePic(1,setShowLoader)
     }, [])
 
-    function switchDevice(val, deviceId) {
-        setShowLoader(true);
-        fetch(`${GATEWAY}${CHANGE_STATE_ENDPOINT}`,
-            {
-                method: 'POST', body: JSON.stringify({ val, deviceId }),
-                headers: { 'Content-Type': 'application/json' }
+    // function switchDevice(val, deviceId) {
+    //     setShowLoader(true);
+    //     fetch(`${GATEWAY}${CHANGE_STATE_ENDPOINT}`,
+    //         {
+    //             method: 'POST', body: JSON.stringify({ val, deviceId }),
+    //             headers: { 'Content-Type': 'application/json' }
 
-            })
-            .then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data); alert(`success - device 1 turned ${val == 1 ? 'on' : 'off'}`) }).catch(() => setShowLoader(false));
-    }
+    //         })
+    //         .then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data); alert(`success - device 1 turned ${val == 1 ? 'on' : 'off'}`) }).catch(() => setShowLoader(false));
+    // }
 
-    function capturePic(cameraId) {
+    // function capturePic(cameraId) {
 
-        setShowLoader(true);
+    //     setShowLoader(true);
 
-        fetch(`${GATEWAY}${CAPTURE_PIC_ENDPOINT}`,
-            {
-                method: 'POST', body: JSON.stringify({ cameraId }),
-                headers: { 'Content-Type': 'application/json' }
-            }).then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data.body); document.querySelector('#c1_view').src = `data:image/png;base64,${data.imageData}` }).catch(() => setShowLoader(false));
+    //     fetch(`${GATEWAY}${CAPTURE_PIC_ENDPOINT}`,
+    //         {
+    //             method: 'POST', body: JSON.stringify({ cameraId }),
+    //             headers: { 'Content-Type': 'application/json' }
+    //         }).then(res => { setShowLoader(false); return res.json() }).then(data => { console.log(data.body); document.querySelector('#c1_view').src = `data:image/png;base64,${data.imageData}` }).catch(() => setShowLoader(false));
 
-    }
+    // }
     return (
         <React.Fragment>
             {showLoader &&
@@ -43,15 +44,15 @@ export default function DeskController() {
             <h3 className="feature_heading">Desk Controls</h3>
             <div>
                 Turn Device 1
-                <button onClick={() => switchDevice(1, 1)}>ON</button>
-                <button onClick={() => switchDevice(0, 1)}>OFF</button>
+                <button onClick={() => switchDevice(1, 1,setShowLoader)}>ON</button>
+                <button onClick={() => switchDevice(0, 1,setShowLoader)}>OFF</button>
             </div>
             <hr />
             <h3 className="feature_heading">Straight from the desk</h3>
             <div>
                 <img id="c1_view" src="image.jpg" alt="My desk from some distance." />
                 <div>
-                    <button onClick={() => capturePic(1)}>Capture now</button>
+                    <button onClick={() => capturePic(1,setShowLoader)}>Capture now</button>
                 </div>
             </div>
         </React.Fragment>
--- public/home_auto/src/modules/features.js
+++ public/home_auto/src/modules/features.js
@@ -5,9 +5,9 @@ export function showLoader(val){
     document.querySelector('#loader').style.display = val ? 'flex' : 'none'
 }   
 
-export function switchDevice(val, deviceId) {
+export function switchDevice(val, deviceId,setShowLoader) {
 
-    showLoader(true);
+    setShowLoader(true);
     
 	fetch(`${GATEWAY}${CHANGE_STATE_ENDPOINT}`,
 		{
@@ -15,18 +15,18 @@ export function switchDevice(val, deviceId) {
 			headers: { 'Content-Type': 'application/json' }
 			
 			})
-		.then(res=>{showLoader(false); return res.json()}).then(data => {console.log(data);alert(`success - device 1 turned ${val==1?'on':'off'}`)}).catch(()=>showLoader(false));
+		.then(res=>{setShowLoader(false); return res.json()}).then(data => {console.log(data);alert(`success - device 1 turned ${val==1?'on':'off'}`)}).catch(()=>setShowLoader(false));
 
 }
 
-export function capturePic(cameraId) {
+export function capturePic(cameraId,setShowLoader) {
 
-    showLoader(true);
+    setShowLoader(true);
 
 	fetch(`${GATEWAY}${CAPTURE_PIC_ENDPOINT}`,
 		{
 			method: 'POST', body: JSON.stringify({ cameraId }),
 			headers: { 'Content-Type': 'application/json' }
-			}).then(res=>{showLoader(false); return res.json()}).then(data => {console.log(data.body); document.querySelector('#c1_view').src=`data:image/png;base64,${data.imageData}`}).catch(()=>showLoader(false));
+			}).then(res=>{setShowLoader(false); return res.json()}).then(data => {console.log(data.body); document.querySelector('#c1_view').src=`data:image/png;base64,${data.imageData}`}).catch(()=>setShowLoader(false));
 
 }
